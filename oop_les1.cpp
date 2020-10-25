f3afda0ef97bd9239a73e6357cc8115d04ee04cc
Sergey Lisin <sergey_lisin@list.ru>
2020-10-25 15:33:41 +0200

    Task2

--- oop_les1.cpp
+++ oop_les1.cpp
@@ -20,12 +20,31 @@ public:
     }
 };
 
+// Task2
+class RGBA{
+private:
+    uint8_t m_red = 0;
+    uint8_t m_green = 0;
+    uint8_t m_blue = 0;
+    uint8_t m_alpha = 255;
+public:
+    RGBA(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha):m_red(red),m_green(green),m_blue(blue),m_alpha(alpha){
+    }
+    void print(){
+        printf("m_red = %d\n",m_red);
+        printf("m_green = %d\n",m_green);
+        printf("m_blue = %d\n",m_blue);
+        printf("m_alpha = %d\n",m_alpha);
+    }
+};
 
-/
 int main(int argc,char** args){
     Power obj1;
     obj1.set(3,4);
     cout << obj1.calculate() << endl;
+    
+    RGBA obj2(110,111,112,113);
+    obj2.print();
 
     
 	return 0;
