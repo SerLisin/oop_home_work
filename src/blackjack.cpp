d330184d2c28d4236347d43a38b1ba0815645908
Sergey Lisin <sergey_lisin@list.ru>
2020-11-03 18:54:46 +0200

    Homework lesson#4, task#3

--- src/blackjack.cpp
+++ src/blackjack.cpp
@@ -1,49 +1,95 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
+#include <vector>
 
+using std::endl;
 using std::cout;
 using std::cin;
-using std::endl;
 using std::string;
+using std::vector;
 
-enum Cardsuit { CLUBS, DIAMONDS, SPADES, HEARTS };
-enum Cardtype {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+
+enum suit { CLUBS, DIAMONDS, SPADES, HEARTS };
+enum rank {ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
 
 
 class Card{
 private:
-    Cardsuit suit;
-    Cardtype type;
-    bool position;
+    suit m_Suit;
+    rank m_Rank;
+    bool m_IsFaceUp;
 public:
-    Card(Cardsuit s, Cardtype t, bool p): suit(s),type(t), position(p){}
-    void Flip(){position = !position;}
+    Card(suit s, rank r, bool p): m_Suit(s),m_Rank(r), m_IsFaceUp(p){}
+    void Flip(){m_IsFaceUp = !m_IsFaceUp;}
     int getValue() const {
-        switch (type) {
-            case ACE   : return 1;
-            case TWO   : return 2;
-            case THREE : return 3;
-            case FOUR  : return 4;
-            case FIVE  : return 5;
-            case SIX   : return 6;
-            case SEVEN : return 7;
-            case EIGHT : return 8;
-            case NINE  : return 9;
-            case TEN   : return 10;
-            case JACK  : return 10;
-            case QUEEN : return 10;
-            case KING  : return 10;
-        }
-        return -1;
-    }
+	    return (m_Rank > 9) ? 10 : m_Rank;
+	}
+};
 
+// Task3
+class Hand{
+private:
+	vector<Card*> m_Cards;
+public:
+	void Add(Card* pCard){m_Cards.push_back(pCard);}
+	void Clear(){m_Cards.clear();}
+	int GetValue(){
+		int sum = 0;
+		vector<Card*>::const_iterator it;
+		it = m_Cards.begin();
+		while(it != m_Cards.end()){
+			if(((*it)->getValue())==1)
+				sum+=(sum+11>21)? 1 : 11;
+			else
+				sum+=(*it)->getValue();
+			++it;
+		}
+		return sum;
+	}
 };
 
 int main(int argc,char** args){
 
-    Card card(SPADES,ACE,true);
-    cout << card.getValue() << endl;
+    cout << "\t\tWelcome to Blackjack!\n\n";
+    
+    int numPlayers = 0;
+    while (numPlayers < 1 || numPlayers > 7)
+    {
+        cout << "How many players? (1 - 7): ";
+        cin >> numPlayers;
+    }
+    
+    vector<string> names;
+    string name;
+    for (int i = 0; i < numPlayers; ++i)
+    {
+        cout << "Enter player name: ";
+        cin >> name;
+        names.push_back(name);
+    }
+    cout << endl;
+    
+    // игровой цикл
+/*    Game aGame(names);
+    char again = 'y';
+    while (again != 'n' && again != 'N')
+    {
+        aGame.Play();
+        cout << "\nDo you want to play again? (Y/N): ";
+        cin >> again;
+    }
+  */  
+
+    Hand hand;  
+	Card card1(SPADES,EIGHT,true);
+	hand.Add(&card1);
+	Card card2(SPADES,THREE,true);
+	hand.Add(&card2);
+	Card card3(SPADES,ACE,true);
+	hand.Add(&card3);
+	
+    cout << "Points: " << hand.GetValue() << endl;
+	hand.Clear();
+    cout << "Points: " << hand.GetValue() << endl;
 
     return 0;
 }
